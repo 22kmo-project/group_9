1a87aee7333c25609a8ed893c03de8866b9b356c
Alisa Kulonpaa <alisa.kulonpaa@gmail.com>
2022-11-27 19:56:44 +0200

    Muutoksia

---
+++ backend/models/card_model.js
@@ -0,0 +1,68 @@
+const db = require("../database");
+const bcrypt = require("bcryptjs");
+
+const saltRounds = 10;
+const card = {
+  get: function (callback) {
+    return db.query("call get_all_card_info()", callback);
+  },
+  getById: function (id, callback) {
+    //return db.query("select * from card where id_card=?", [id], callback);
+    return db.query("call get_card_info(?)", [id], callback);
+  },
+  add: function (card, callback) {
+    bcrypt.hash(card.pin, saltRounds, function (err, hash) {
+      console.log(
+        hash +
+          "\n" +
+          card.owner_id +
+          "\n" +
+          card.account_id +
+          "\n" +
+          card.card_type
+      );
+
+      //return db.query("insert into card ( pin) values(?)", [hash], callback);
+      return db.query(
+        "call create_card(?,?,?,?)",
+        [hash, card.owner_id, card.account_id, card.card_type],
+        callback
+      );
+    });
+  },
+  delete: function (id, callback) {
+    return db.query("delete from card where id_card=?", [id], callback);
+  },
+  update: function (id, card, callback) {
+    bcrypt.hash(card.pin, saltRounds, function (err, hash) {
+      return db.query(
+        "update card set pin=? where id_card=?",
+        [hash, id],
+        callback
+      );
+    });
+  },
+
+  checkPassword: function (id_card, callback) {
+    /*db.query(
+      "select pin from Card where id_card=? ",
+      [id_card],
+      function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        callback(result);
+      }
+    );*/
+
+    //funvtion sql
+    //"call function(?,?)", [id], [numer]
+
+    return db.query(
+      "select pin from card where id_card=?",
+      [id_card],
+      callback
+    );
+  },
+};
+
+module.exports = card;
---
+++ backend/routes/login.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const login = require("../models/card_model");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+router.post("/", function (request, response) {
+  if (request.body.id_card && request.body.pin) {
+    const id_card = request.body.id_card;
+    console.log("id_card " + id_card);
+
+    const pin = request.body.pin;
+    console.log("pin " + id_card);
+
+    login.checkPassword(id_card, function (dbError, dbResult) {
+      if (dbError) {
+        response.json(dbError);
+      } else {
+        console.log(dbResult[0].pin + " " + pin);
+
+        if (dbResult.length > 0) {
+          bcrypt.compare(pin, dbResult[0].pin, function (err, compareResult) {
+            if (compareResult) {
+              console.log("succes");
+              const token = generateAccessToken({
+                username: request.body.id_card,
+              });
+              response.send(token);
+            } else {
+              console.log("wrong pin");
+              response.send(false);
+            }
+          });
+        } else {
+          console.log("id_card does not exists");
+          response.send(false);
+        }
+      }
+    });
+  } else {
+    console.log("id_card or pin missing");
+    response.send(false);
+  }
+});
+
+function generateAccessToken(username) {
+  dotenv.config();
+  return jwt.sign(username, process.env.MY_TOKEN, {
+    expiresIn: "1800s",
+  });
+}
+
+module.exports = router;
